3c9f055f67295370656781129229fc25a8e1dfd4
sheenrox82 <sheenrox82@yahoo.com>
2014-08-05 21:19:19 -0500

    Unified workspace, and stuff, removal of unused imports and variables, etc.

--- sheenrox82/RioV/src/api/item/RioVItem.java
+++ sheenrox82/RioV/src/api/item/RioVItem.java
@@ -10,10 +10,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+
+import org.lwjgl.input.Keyboard;
+
 import sheenrox82.RioV.src.api.base.RioVAPI;
-import sheenrox82.RioV.src.api.util.Color;
-import sheenrox82.RioV.src.api.util.RioVAPIUtil;
-import sheenrox82.RioV.src.api.util.RioVPlayer;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -41,17 +41,30 @@ public class RioVItem extends Item
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister par1IconRegister)
 	{
-		this.itemIcon = par1IconRegister.registerIcon(RioVAPI.getInstance().getUtil().mod_id + ":" + RioVAPIUtil.getName(this.getUnlocalizedName()));
+		this.itemIcon = par1IconRegister.registerIcon(RioVAPI.getInstance().getUtil().mod_id + ":" + RioVAPI.getInstance().getUtil().getName(this.getUnlocalizedName()));
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4)
 	{
-		if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("amethyst")))par3List.add("A gem to show wealth and power");
-		if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("onyx")))par3List.add("A dark mass");
-		if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("vravinite")))par3List.add("A crystal to show confidence");
-		if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("aleris")))par3List.add("A gem to show defence and strategies");
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+		{
+			if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("amethyst")))par3List.add("A gem to show wealth and power");
+			if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("onyx")))par3List.add("A dark mass");
+			if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("vravinite")))par3List.add("A crystal to show confidence");
+			if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("aleris")))par3List.add("A gem to show defence and strategies");
+		}
+		else
+		{
+			if(par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("amethyst")) ||
+					par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("onyx")) ||
+					par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("vravinite")) ||
+					par1ItemStack.getItem().equals(RioVAPI.getInstance().getUtil().getRioVItem("aleris")))
+			{
+				par3List.add("[SHIFT]");
+			}
+		}
 	}
 
 	@Override
@@ -82,7 +95,7 @@ public class RioVItem extends Item
 				attributeInstance.applyModifier(moreHealth);
 			}
 		}
-	
+
 		return par1ItemStack;
 	}
 }
